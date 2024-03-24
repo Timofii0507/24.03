ef52915dee7c5390a1d58397a1eab0702c6fee80
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-24 19:46:04 +0200

    Шосте завдання  з  практичної роботи

--- Program.cs
+++ Program.cs
@@ -6,26 +6,24 @@ using System.Threading.Tasks;
 
 namespace _24._03
 {
-    public static class StringExtensions
+    class Program
     {
-        public static int CountUkrainianConsonants(this string input)
-        {
-            return input.Count(c => "бвгґджзйклмнпрстфхцчшщ".Contains(Char.ToLower(c)));
-        }
-    }
-
-    public class Program
-    {
-        public static void Main()
+        static void Main()
         {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
             Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.CursorVisible = false;
-            Action<string> onConsonantsCounted = (s) => Console.WriteLine($"У рядку '{s}' {s.CountUkrainianConsonants()} українських приголосних.");
+            StringExtensions.SentenceCounted += OnSentenceCounted;
 
-            string testString = "Привіт, Світ!";
-            onConsonantsCounted(testString);
+            string text = "Це тестовий рядок. Він містить кілька речень! Чи працює метод?";
+            int sentenceCount = text.CountSentences();
+            Console.WriteLine($"Кількість речень у рядку: {sentenceCount}");
+        }
+
+        private static void OnSentenceCounted(object sender, SentenceCountedEventArgs e)
+        {
+            Console.WriteLine($"Підрахунок завершено. Кількість речень: {e.Count}");
         }
     }
 }
---
+++ SentenceCountedHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._03
+{
+    public class SentenceCountedEventArgs : EventArgs
+    {
+        public int Count { get; }
+
+        public SentenceCountedEventArgs(int count)
+        {
+            Count = count;
+        }
+    }
+}
