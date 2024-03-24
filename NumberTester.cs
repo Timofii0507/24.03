7c2c0a855b641e01b0ef5046c02a802ff9e78e60
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-24 13:06:36 +0200

    Перше завдання з  практичної  роботи

---
+++ NumberTester.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._03
+{
+    public delegate void NumberTestedEventHandler(int number, bool isOdd);
+
+    public class NumberTester
+    {
+        public event NumberTestedEventHandler NumberTested;
+
+        public void TestNumber(int number)
+        {
+            bool isOdd = number.IsOdd();
+            OnNumberTested(number, isOdd);
+        }
+
+        protected virtual void OnNumberTested(int number, bool isOdd)
+        {
+            NumberTested?.Invoke(number, isOdd);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            var tester = new NumberTester();
+            tester.NumberTested += (number, isOdd) =>
+            {
+                Console.WriteLine($"Число {number} є " + (isOdd ? "непарним." : "парним."));
+            };
+
+            tester.TestNumber(5);
+            tester.TestNumber(8);
+        }
+    }
+}
